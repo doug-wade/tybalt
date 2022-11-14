d6984e4828e1509e22dfe5c2fdd35c8fba9ee132
Douglas Wade <douglas.b.wade@gmail.com>
2022-11-14 15:46:03 -0800

    Add code coverage action

--- packages/cli/src/commands/test.ts
+++ packages/cli/src/commands/test.ts
@@ -4,6 +4,7 @@ export default ({ program }: CommandContext) => {
     program.command('test')
         .description('test a component or components')
         .argument('[string]', 'pattern', 'src/**/*.test.{ts|js}')
+        .option('--coverage', 'whether to generate test coverage', false)
         .action((pattern: string) => {
             console.log(`called test with pattern ${pattern}`);
         });
