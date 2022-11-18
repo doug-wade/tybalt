6a05b7d86baa53a495a3f3fda93003a18102488a
Douglas Wade <douglas.b.wade@gmail.com>
2022-11-17 23:25:29 -0800

    add hello world website

---
+++ packages/eleventy-plugin/index.js
@@ -0,0 +1,1 @@
+console.log("gambit eleventy plugin loaded");
---
+++ packages/website/.eleventy.js
@@ -0,0 +1,9 @@
+// const gambitPlugin = require("@gambit/eleventy-plugin");
+
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addPassthroughCopy("./components");
+  eleventyConfig.addPassthroughCopy("./lib");
+  //   eleventyConfig.addPlugin(gambitPlugin, {
+  //     componentModules: ["./components/index.js"],
+  //   });
+};
