a878e03b0100da19c06e9522edd97ca8b8b003cc
Douglas Wade <douglas.b.wade@gmail.com>
2022-11-20 21:10:50 -0800

    Add build to cli

--- packages/cli/src/commands/build.ts
+++ packages/cli/src/commands/build.ts
@@ -1,20 +1,36 @@
+import { transform } from "@swc/core";
+import glob from 'glob';
+import mkdirp from 'mkdirp';
+import fs from 'node:fs';
+import path from 'node:path';
+
 import type { CommandContext } from '../types';
+
+const globPromise = (pattern: string, options = {}): Promise<string[]> => {
+  return new Promise((resolve, reject) => {
+    glob(pattern, options, (err, files) => err === null ? resolve(files) : reject(err))
+  })
+}
 
 export default ({ program }: CommandContext) => {
     program.command('build')
         .description('build a component or components')
-        .option('-e, --examples', './components/**/*.examples.ts')
-        .option('-c, --components', './components/**/*.component.ts')
-        .option('-w, --website', './components/**/*.page.ts')
-        .action((options) => {
-            if (options.examples) {
-                console.log(`building examples at path ${options.examples}...`);
+        .argument('[string]', 'pattern', 'src/**/*.component.{ts,js}')
+        .option('-d, --directory <string>', 'the output directory', 'dist')
+        .action(async (pattern: string, options) => {
+            const files = await globPromise(pattern);
+
+            if (!files.length) {
+              console.error(`pattern ${pattern} did not match any files`);
             }
-            if (options.components) {
-                console.log(`building components at path ${options.components}`);
-            }
-            if (options.website) {
-                console.log(`building website at path ${options.website}`);
-            }
+
+            files.forEach(async (filename) => {
+              console.log(`compiling file ${filename}`);
+              const sourceCode = await fs.promises.readFile(filename);
+              const output = await transform(String(sourceCode), { filename });
+              const outFilename = filename.replace('src', options.directory);
+              await mkdirp(path.dirname(outFilename));
+              fs.promises.writeFile(outFilename, output.code);
+            });
         });
 };
