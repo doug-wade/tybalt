6e2c6b1229ebcc8e76d8f628bb02c600d8182e49
doug-wade <doug-wade@users.noreply.github.com>
2022-11-22 01:16:04 +0000

    deploy: 59c8084a2938e715d8809f6b543dd01971a5cab0

--- components/footer.js
+++ components/footer.js
@@ -1,6 +1,6 @@
-import { defineComponent } from "../lib/tybalt-core/index.js";
+import { defineComponent, html } from "../lib/tybalt-core/index.js";
 
 defineComponent({
   name: "tybalt-footer",
-  template: "<footer>Tybalt distributed under the MIT license</footer>",
+  template: html`<footer>Tybalt distributed under the MIT license</footer>`,
 });
