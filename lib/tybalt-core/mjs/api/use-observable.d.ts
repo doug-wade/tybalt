4fd3b4d4b6f37c8d39f64d5ecd69bdf950cfbc9b
doug-wade <doug-wade@users.noreply.github.com>
2022-12-15 21:56:16 +0000

    deploy: 28a357f99240db3d4d4e7a8efd8348d8906f2348

---
+++ lib/tybalt-core/mjs/api/use-observable.d.ts
@@ -0,0 +1,7 @@
+import 'core-js/proposals/observable';
+import type { useObservableOptions } from '../types';
+declare const _default: ({ initialValue, subscriber }: useObservableOptions) => {
+    handler: (event: any) => void;
+    observable: any;
+};
+export default _default;
