66e67d85ca5c2a772a97c69e7cdcfa826a846a7b
Douglas Wade <douglas.b.wade@gmail.com>
2022-12-24 09:06:39 -0800

    feat(NOBUG): Add youtube embed example (#143)

---
+++ packages/example/src/YoutubeEmbed/youtube-embed.component.ts
@@ -0,0 +1,25 @@
+import { defineComponent, html } from '@tybalt/core';
+import { compose, string, required } from '@tybalt/validator';
+
+export default defineComponent({
+    name: 'example-youtube-embed',
+    shadowMode: 'open',
+    props: {
+        youtubeId: {
+            validator: compose(required(), string()),
+            default: '1RlI-JxwPNk'
+        }
+    },
+    render({ youtubeId }: { youtubeId: string }) {
+        return html`
+            <iframe 
+                width="560" 
+                height="315" 
+                src="https://www.youtube.com/embed/${youtubeId}" 
+                title="YouTube video player" 
+                frameborder="0" 
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+                allowfullscreen
+            ></iframe>` 
+    }
+});
--- packages/example/src/index.ts
+++ packages/example/src/index.ts
@@ -2,3 +2,4 @@ import "./Button/button.component";
 import "./CookieBanner/cookie-banner.component";
 import "./Link/link.component";
 import "./Circle/circle.component";
+import "./YoutubeEmbed/youtube-embed.component";
