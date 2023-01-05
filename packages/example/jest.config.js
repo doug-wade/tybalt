5f81da6f982aaa3ac02a2c59235bd5dd9bdd9089
Douglas Wade <douglas.b.wade@gmail.com>
2023-01-04 22:40:50 -0800

    fix(#121): mock css imports

--- packages/example/jest.config.js
+++ packages/example/jest.config.js
@@ -3,6 +3,7 @@ export default {
     extensionsToTreatAsEsm: ['.ts'],
     transform: {
         '^.+\\.tsx?$': 'esbuild-jest',
+        '^.+\\.css$': '<rootDir>/__mocks__/style-mock.ts',
     },
     testEnvironment: 'jest-environment-jsdom',
     setupFilesAfterEnv: ['../../node_modules/@tybalt/test-utils/dist/cjs/setup.js', '<rootDir>/jest.setup.js'],
