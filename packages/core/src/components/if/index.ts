749f226f99cc6e82fbdc495740f726113a850f53
Douglas Wade <douglas.b.wade@gmail.com>
2023-01-05 21:12:18 -0800

    feat(#164): typescript bullshit

--- packages/core/src/components/if/index.ts
+++ packages/core/src/components/if/index.ts
@@ -2,7 +2,7 @@ import { defineComponent, html } from '../..';
 
 export default defineComponent({
     name: 't-if',
-    render({ condition }) {
+    render({ condition }: { condition: any }) {
         let slotName = condition ? 'then' : 'else';
         return html`<slot name="${slotName}"></slot>`
     }
