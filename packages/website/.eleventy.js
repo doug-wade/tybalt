e8b070bd2932c6ac52cb3a05b907b08efcce080d
Douglas Wade <douglas.b.wade@gmail.com>
2023-01-07 20:39:18 -0800

    fix(#174): switch the website to use a cdn (#175)

--- packages/website/.eleventy.js
+++ packages/website/.eleventy.js
@@ -3,6 +3,7 @@
 module.exports = function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy('./components');
     eleventyConfig.addPassthroughCopy('./lib');
+    eleventyConfig.addPassthroughCopy('./css');
     eleventyConfig.setBrowserSyncConfig({
         open: true,
     });
@@ -13,5 +14,6 @@ module.exports = function (eleventyConfig) {
 
     return {
         pathPrefix: '/tybalt/',
+        passthroughFileCopy: true,
     };
 };
--- packages/website/components/main.js
+++ packages/website/components/main.js
@@ -1,8 +1,8 @@
 import { defineComponent, html } from 'https://unpkg.com/@tybalt/core@0.0.10/dist/mjs/index.js';
 
 defineComponent({
-    name: 'tybalt-footer',
+    name: 'tybalt-main',
     render() {
-        return html`<footer>Tybalt distributed under the MIT license</footer>`;
+        return html`<div>Tutorial coming here soon!</div>`;
     },
 });
