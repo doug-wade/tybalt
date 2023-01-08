d1b66e80071a93996c0564ea53b141351f6fa8b1
Douglas Wade <douglas.b.wade@gmail.com>
2023-01-07 21:03:15 -0800

    Revert "Revert "fix(#174): switch the website to use a cdn (#175)" (#176)"
    This reverts commit a8f3f33ee765c0cfa0c3bc55925f40480268374f.

--- packages/website/components/header.js
+++ packages/website/components/header.js
@@ -1,16 +1,18 @@
-import { css, defineComponent, html } from '../lib/tybalt-core/index.js';
+import { defineComponent, html } from 'https://unpkg.com/@tybalt/core@0.0.10/dist/mjs/index.js';
 
 defineComponent({
     name: 'tybalt-header',
-    template: html`
-        <nav>
-            <span>Tybalt Web Components</span>
-            <span>
-                <a href="https://github.com/doug-wade/tybalt">Github</a>
-            </span>
-        </nav>
-    `,
-    css: css`
+    render() {
+        return html`
+            <nav>
+                <span>Tybalt Web Components</span>
+                <span>
+                    <tybalt-link href="https://github.com/doug-wade/tybalt">Github</tybalt-link>
+                </span>
+            </nav>
+        `;
+    },
+    css: `
         nav {
             display: flex;
             justify-content: space-between;
