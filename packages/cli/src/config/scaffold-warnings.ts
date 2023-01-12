9d55e3e40c9a2cee18f45139601a1a3d3371a67a
Douglas Wade <douglas.b.wade@gmail.com>
2023-01-11 20:16:20 -0800

    fix(#180): Add scaffold command

---
+++ packages/cli/src/config/scaffold-warnings.ts
@@ -0,0 +1,3 @@
+export default new Map([
+    ['jest.config.js', 'cannot merge Jest configs; please refer to docs for how to set up Jest'],
+])
--- packages/cli/src/templates/component.ts
+++ packages/cli/src/templates/component.ts
@@ -2,24 +2,14 @@ import type { ScaffoldContext } from '../types';
 
 export default ({ kebabCaseName }: ScaffoldContext) => {
     return `
-        import { defineComponent, reactive } from '@tybalt/core';
-        import { string } from '@tybalt/validator';
+        import { defineComponent, html } from '@tybalt/core';
+        import { compose, string, required } from '@tybalt/validator';
 
         export default defineComponent({
             name: '${kebabCaseName}',
-            emits: ['increment'],
-            props: { validator: string() },
-            setup(props, ctx) {
-                const count = reactive(0);
-                const clickHandler = () => { 
-                    count.value += 1;
-                    this.emit('increment', count.value);
-                };
-
-                return {
-                    count,
-                    clickHandler
-                };
+            props: { name: { validator: compose(string(), required) } },
+            render({ name }, ctx) {
+                return html\`<div class="$\{kebabCaseName\}">Hello $\{name\}</div>\`;
             }
         });
     `;
