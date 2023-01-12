991c7dd35bc4b67b9d5d072955f0fcb349ec4266
Douglas Wade <douglas.b.wade@gmail.com>
2023-01-11 21:43:30 -0800

    feat: Add more website styles (#186)

--- packages/website/.eleventy.js
+++ packages/website/.eleventy.js
@@ -2,6 +2,7 @@ const tybaltPlugin = require('@tybalt/eleventy-plugin');
 
 module.exports = function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy('./css');
+    eleventyConfig.addPassthroughCopy('./img');
     eleventyConfig.setBrowserSyncConfig({
         open: true,
     });
