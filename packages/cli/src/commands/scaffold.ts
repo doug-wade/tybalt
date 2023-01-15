f1d19a0af467c66652b5ef94889ee694f48e7492
Douglas Wade <douglas.b.wade@gmail.com>
2023-01-15 12:27:24 -0800

    feat(#180): Add scaffold command

--- packages/cli/src/commands/scaffold.ts
+++ packages/cli/src/commands/scaffold.ts
@@ -8,12 +8,14 @@ import util from 'node:util';
 
 import WARNINGS from '../config/scaffold-warnings.js';
 import { SCAFFOLD_TARGETS } from '../config/constants.js';
-import componentTemplate from '../templates/component.js';
-import stylesTemplate from '../templates/styles.js';
-import testsTemplate from '../templates/tests.js';
-import jestConfigTemplate from '../templates/jest-config.js';
-import indexHtmlTemplate from '../templates/index-html.js';
-import eleventyConfigTemplate from '../templates/eleventy-config.js';
+import {
+    componentTemplate,
+    stylesTemplate,
+    testsTemplate,
+    jestConfigTemplate,
+    indexHtmlTemplate,
+    eleventyConfigTemplate,
+} from '../templates';
 
 import { ScaffoldContext, ScaffoldCommandOptions, CommandContext, ScaffoldTarget } from '../types';
 
@@ -32,24 +34,18 @@ const execAsync = async (command: string) => {
         console.log(stderr);
         console.log();
     }
-}
+};
 
 // We want to run npm i as few times as possible so that npm spends as little time calculating
 // the ideal dependency closure as possible, and can parallelize as much as possible.
-const dependencies = [
-    '@tybalt/core',
-    '@tybalt/validator'
-];
-const devDependencies = [
-    '@tybalt/esbuild-plugin',
-    'esbuild'
-];
-const scripts: { [property: string]: { script: string, description: string } } = {};
+const dependencies = ['@tybalt/core', '@tybalt/validator'];
+const devDependencies = ['@tybalt/esbuild-plugin', 'esbuild'];
+const scripts: { [property: string]: { script: string; description: string } } = {};
 
 // Installs all the dependencies
 const installDependencies = async () => {
     console.log('installing dependencies; there may be errors');
-    
+
     await execAsync(`npm i -S ${dependencies.join(' ')}`);
     await execAsync(`npm i -D ${devDependencies.join(' ')}`);
 };
@@ -58,24 +54,34 @@ const installDependencies = async () => {
 const writeScripts = async () => {
     console.log('adding the following scripts to package.json');
 
-    await Promise.all(Object.entries(scripts).map(([scriptName, { script, description }]) => {
-        execAsync(`npm pkg set 'scripts.${scriptName}'='${script}'`);
-        console.log(`    ${scriptName}: ${description}`);
-    }));
+    await Promise.all(
+        Object.entries(scripts).map(([scriptName, { script, description }]) => {
+            execAsync(`npm pkg set 'scripts.${scriptName}'='${script}'`);
+            console.log(`    ${scriptName}: ${description}`);
+        }),
+    );
 
     console.log('\r\n');
 };
 
 // Don't let the name confuse you, this is mostly about calculating warning messages to emit when we don't write files
 // (it also writes files).
-const writeFile = async ({ filePath, fileContents, warning }: { filePath: string, fileContents: string, warning?: string }) => {
+const writeFile = async ({
+    filePath,
+    fileContents,
+    warning,
+}: {
+    filePath: string;
+    fileContents: string;
+    warning?: string;
+}) => {
     if (fs.existsSync(filePath)) {
         console.warn(warning || WARNINGS.get(filePath) || `file ${filePath} already exists; refusing to overwrite`);
         return;
     }
 
     return fs.promises.writeFile(filePath, fileContents);
-}
+};
 
 // Validates a web component name. Probably this should be in @tybalt/core, tbh.
 const validateName = ({ componentName }: { componentName: string }) => {
@@ -99,11 +105,11 @@ const makeScaffoldingContext = ({ name }: { name: string }): ScaffoldContext => 
 };
 
 // Scaffolding steps general to scaffolding any kind of project (fastify, eleventy, etc).
-const scaffoldProject = async ({ projectName, options }: { projectName: string, options: ScaffoldCommandOptions }) => {
+const scaffoldProject = async ({ projectName, options }: { projectName: string; options: ScaffoldCommandOptions }) => {
     // Create a safe workspace to work in to avoid stomping any current contents of the file system.
     if (fs.existsSync(projectName)) {
         throw new Error(
-            `project directory ${projectName} already exists; please delete it or move it away before proceeding.`
+            `project directory ${projectName} already exists; please delete it or move it away before proceeding.`,
         );
     }
 
@@ -118,8 +124,8 @@ const scaffoldProject = async ({ projectName, options }: { projectName: string, 
 
         writeFile({
             filePath: `jest.config.js`,
-            fileContents: jestConfigTemplate(), 
-            warning: 'jest config cannot be written as it would overwrite an existing file' 
+            fileContents: jestConfigTemplate(),
+            warning: 'jest config cannot be written as it would overwrite an existing file',
         });
 
         scripts['test'] = { script: 'jest', description: 'run the unit tests' };
@@ -127,7 +133,7 @@ const scaffoldProject = async ({ projectName, options }: { projectName: string, 
 };
 
 // Scaffolding steps specific for scaffolding an eleventy site
-const scaffoldEleventy = async ({ projectName, options }: { projectName: string, options: ScaffoldCommandOptions }) => {
+const scaffoldEleventy = async ({ projectName, options }: { projectName: string; options: ScaffoldCommandOptions }) => {
     devDependencies.push('@tybalt/eleventy-plugin');
     devDependencies.push('@11ty/eleventy');
 
@@ -135,13 +141,13 @@ const scaffoldEleventy = async ({ projectName, options }: { projectName: string,
 
     writeFile({
         filePath: `.eleventy.js`,
-        fileContents: eleventyConfigTemplate(), 
-        warning: 'jest config cannot be written as it would overwrite an existing file' 
+        fileContents: eleventyConfigTemplate(),
+        warning: 'jest config cannot be written as it would overwrite an existing file',
     });
     writeFile({
         filePath: `index.html`,
-        fileContents: indexHtmlTemplate(context), 
-        warning: 'index.html cannot be written as it would overwrite an existing file' 
+        fileContents: indexHtmlTemplate(context),
+        warning: 'index.html cannot be written as it would overwrite an existing file',
     });
 
     scripts['build'] = { script: 'eleventy', description: 'build the static website' };
@@ -149,8 +155,15 @@ const scaffoldEleventy = async ({ projectName, options }: { projectName: string,
 };
 
 // Scaffolding steps specific for scaffolding a fastify site
-const scaffoldFastify = async ({ projectName, options }: { projectName: string, options: ScaffoldCommandOptions }) => {
-    dependencies.push('@fastify/autoload', '@fastify/sensible', '@fastify/static','fastify', 'fastify-cli', 'fastify-plugin');
+const scaffoldFastify = async ({ projectName, options }: { projectName: string; options: ScaffoldCommandOptions }) => {
+    dependencies.push(
+        '@fastify/autoload',
+        '@fastify/sensible',
+        '@fastify/static',
+        'fastify',
+        'fastify-cli',
+        'fastify-plugin',
+    );
     devDependencies.push('@11ty/eleventy');
 
     scripts['start'] = { script: 'fastify start -l info app.js', description: 'start the production server' };
@@ -158,7 +171,13 @@ const scaffoldFastify = async ({ projectName, options }: { projectName: string, 
 };
 
 // Scaffolding steps specific to scaffolding a component
-const scaffoldComponent = async ({ componentName, options }: { componentName: string, options: ScaffoldCommandOptions }) => {
+const scaffoldComponent = async ({
+    componentName,
+    options,
+}: {
+    componentName: string;
+    options: ScaffoldCommandOptions;
+}) => {
     validateName({ componentName });
     const context = makeScaffoldingContext({ name: componentName });
     const componentDirectory = path.resolve(`./components/${context.pascalCaseName}`);
@@ -168,38 +187,38 @@ const scaffoldComponent = async ({ componentName, options }: { componentName: st
     if (options.implementation) {
         writeFile({
             filePath: `${componentDirectory}/${context.kebabCaseName}.ts`,
-            fileContents: componentTemplate(context), 
-            warning: 'Component implementation cannot be written as it would overwrite an existing file' 
+            fileContents: componentTemplate(context),
+            warning: 'Component implementation cannot be written as it would overwrite an existing file',
         });
     }
 
     if (options.styles) {
         writeFile({
             filePath: `${componentDirectory}/${context.kebabCaseName}.css`,
-            fileContents: stylesTemplate(context), 
-            warning: 'Component styles cannot be written as it would overwrite an existing file' 
+            fileContents: stylesTemplate(context),
+            warning: 'Component styles cannot be written as it would overwrite an existing file',
         });
     }
 
     if (options.tests) {
         writeFile({
             filePath: `${componentDirectory}/${context.kebabCaseName}.test.ts`,
-            fileContents: testsTemplate(context), 
-            warning: 'Component unit tests cannot be written as it would overwrite an existing file' 
+            fileContents: testsTemplate(context),
+            warning: 'Component unit tests cannot be written as it would overwrite an existing file',
         });
     }
-}
+};
 
 // Coerce the argument to a ScaffoldTarget; throw if it fails
 const validateTarget = (target: string): ScaffoldTarget => {
     const cleanedTarget = target.trim().toLowerCase();
 
     if (Object.values(SCAFFOLD_TARGETS).some((potentialTarget) => cleanedTarget === potentialTarget)) {
-        return <ScaffoldTarget> cleanedTarget;
+        return <ScaffoldTarget>cleanedTarget;
     } else {
         throw new Error(`Must choose one of: ${Object.values(SCAFFOLD_TARGETS).join(', ')}; got ${target}`);
     }
-}
+};
 
 const action = async (targetArg: string, options: ScaffoldCommandOptions) => {
     const target = validateTarget(targetArg);
