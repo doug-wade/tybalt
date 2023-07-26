40ce0f9e5da0665e54ddc83e4288ce1d71753f4e
Aidin Givtaj <givtaj@gmail.com>
2023-07-26 11:26:41 +0200

    feat: use package.json version in tests (#310)

--- packages/cli/tst/index.test.ts
+++ packages/cli/tst/index.test.ts
@@ -1,12 +1,13 @@
 import { describe, it, expect } from '@jest/globals';
 import child_process from 'node:child_process';
 import path from 'node:path';
+import packageJson from "../package.json"
 
 describe('cli', () => {
     it('should emit the version', () => {
         const binScriptPath = path.resolve(`${__dirname}/../src/index.ts`);
         const results = child_process.execSync(`npx ts-node-esm ${binScriptPath} --version`);
 
-        expect(results.toString()).toEqual('0.0.12\n');
+        expect(results.toString()).toEqual(`${packageJson.version}\n`);
     });
 });
