e31e6b30f194f541f31c95c21fbed9f71fab97dc
Ashvith Shetty <113123021+Ashvith10@users.noreply.github.com>
2023-08-05 20:51:36 +0530

    feat: Add flag to allow overriding of port

--- packages/cli/src/commands/serve.ts
+++ packages/cli/src/commands/serve.ts
@@ -7,11 +7,13 @@ export default ({ program }: CommandContext) => {
         .description('watch a component or components')
         .option('-e, --examples', 'whether to serve the examples', true)
         .option('-w, --website', 'whether to serve the website', true)
-        .action((options: { examples: boolean; website: boolean }) => {
+        .option('-p, --port <value>', 'which port to serve', '3000')
+        .action((options: { examples: boolean; website: boolean; port: string }) => {
             const server = bs.create();
-
+        
             bs.init({
                 server: './dist',
+                port: Number(options.port)
             });
 
             bs.reload('*.html');
