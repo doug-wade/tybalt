65d46024b506da6454e541b64536de331c558e86
Douglas Wade <douglas.b.wade@gmail.com>
2023-12-08 15:50:57 -0800

    feat(#205): Add code example component

---
+++ packages/website/components/code-example.js
@@ -0,0 +1,38 @@
+import { defineComponent, html } from '@tybalt/core';
+import { compose, string, required } from '@tybalt/validator';
+
+import Prism from 'prismjs/prism.js';
+import syntaxHighlighting from '../../../node_modules/prism-themes/themes/prism-night-owl.css' assert { type: "css" };
+
+function encode (str) { 
+    var buf = [];
+    for (var i = str.length - 1; i >= 0; i--) {
+      buf.unshift(["&#", str[i].charCodeAt(), ";"].join(""));
+    }
+    return buf.join("");
+} 
+
+defineComponent({
+    name: 'tybalt-code-example',
+    shadowMode: 'open',
+    css: syntaxHighlighting,
+    props: {
+        language: {
+            validator: compose(string, required)
+        }
+    },
+    setup({ language }) {
+        const code = this.innerHTML.replace(/^\s+/g, "");
+        const languageCode = language === 'shell' ? Prism.languages.shell : Prism.languages.javascript
+        const highlighted = Prism.highlight(code, languageCode, language);
+
+        return {
+            code: highlighted,
+        }
+    },
+    render({ code, language }) {
+        return html`
+            <pre><code class="language-${language}">${code}</code></pre>
+        `;
+    },
+});
