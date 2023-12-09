b2598be4527565ab6eacc42c0bd2b7694376d448
Douglas Wade <douglas.b.wade@gmail.com>
2023-12-08 16:12:42 -0800

    fix(#118): Implement the cli watch command

--- packages/example/src/index.ts
+++
@@ -1,8 +0,0 @@
-import './BrewerySearch/brewery-search.component';
-import './Button/button.component';
-import './Circle/circle.component';
-import './ClosedShadowDom/ClosedShadowDom.component';
-import './Counter/counter.component';
-import './CookieBanner/cookie-banner.component';
-import './Link/link.component';
-import './YoutubeEmbed/youtube-embed.component';
