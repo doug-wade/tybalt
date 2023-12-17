349089d31712105840b231e979dc843fedd9c17b
Douglas Wade <douglas.b.wade@gmail.com>
2023-12-17 15:12:15 -0800

    feat(#134): Add reactive documentation to the sidebar

--- packages/website/components/sidebar.js
+++ packages/website/components/sidebar.js
@@ -1,6 +1,6 @@
 import { defineComponent, html } from '@tybalt/core';
 
-const PACKAGES = ['cli', 'core', 'eleventy-plugin', 'esbuild-plugin', 'eslint-plugin', 'parser', 'test-utils', 'validator'];
+const PACKAGES = ['cli', 'core', 'eleventy-plugin', 'esbuild-plugin', 'eslint-plugin', 'parser', 'reactive', 'test-utils', 'validator'];
 const GUIDES = ['props', 'events', 'slots', 'new-website', 'styling-your-component', 'writing-tests', 'custom-validator', 'data-fetching', 'linting', 'building'];
 
 defineComponent({
