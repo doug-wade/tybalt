3a5c258549c9e74fab1e4cfb0ddbc11ef7b2f823
Douglas Wade <douglas.b.wade@gmail.com>
2024-01-10 20:58:49 -0800

    fix(#193): Update cypress tests

--- packages/cypress/e2e/website/guides.cy.js
+++ packages/cypress/e2e/website/guides.cy.js
@@ -5,7 +5,7 @@ const guides = ['custom-validator', 'new-website', 'styling-your-component', 'wr
 guides.forEach((guide) => {
     describe(`guide: ${guide}`, () => {
         beforeEach(() => {
-            cy.visit(`http://localhost:8081/pages/${guide}-guide/`);
+            cy.visit(`http://localhost:8081/guide/${guide}/`);
         });
 
         it('should mount the layout', () => {
