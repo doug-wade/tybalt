f3d33017753aa9d1920ca86de73603d8fd21b7c3
Douglas Wade <douglas.b.wade@gmail.com>
2024-01-13 19:33:42 -0800

    fix(NOBUG): remove stray deprecated argument

--- packages/esbuild-plugin/src/index.ts
+++ packages/esbuild-plugin/src/index.ts
@@ -3,17 +3,19 @@ import type { PluginBuild } from 'esbuild';
 import fs from 'node:fs';
 import path from 'node:path';
 
+const namespace = 'tybalt-styles';
+
 export default () => ({
     name: 'tybalt',
     setup(build: PluginBuild) {
         build.onResolve({ filter: /.*\.css$/ }, (args) => {
             return {
                 path: path.resolve(args.resolveDir, args.path),
-                namespace: 'tybalt-styles',
+                namespace,
             };
         });
 
-        build.onLoad({ filter: /.*/, namespace: 'tybalt-styles' }, async (args) => {
+        build.onLoad({ filter: /.*/, namespace }, async (args) => {
             const contents = await fs.promises.readFile(args.path);
             return {
                 contents,
