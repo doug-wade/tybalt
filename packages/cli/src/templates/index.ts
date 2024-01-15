54661dfc3a6d644f5b67a7261a85784d56fa34d1
Douglas Wade <douglas.b.wade@gmail.com>
2024-01-15 11:03:07 -0800

    fix(#601): resolve missing import

--- packages/cli/src/templates/index.ts
+++ packages/cli/src/templates/index.ts
@@ -1,6 +1,5 @@
 export { default as componentTemplate } from './component.js';
 export { default as stylesTemplate } from './styles.js';
 export { default as testsTemplate } from './tests.js';
-export { default as jestConfigTemplate } from './jest-config.js';
 export { default as indexHtmlTemplate } from './index-html.js';
 export { default as eleventyConfigTemplate } from './eleventy-config.js';
